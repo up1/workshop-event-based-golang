250ae237bba6444ee8c9deb25f2d5831792b4e09
up1 <somkiat.p@gmail.com>
2025-10-30 23:22:24 +0700

    Add pact

---
+++ contract_test/consumer1/client_pact_test.go
@@ -0,0 +1,121 @@
+//go:build integration
+
+package consumer1_test
+
+import (
+	"fmt"
+	"model"
+	"os"
+	"strconv"
+	"testing"
+
+	"consumer1"
+	"net/url"
+
+	"github.com/pact-foundation/pact-go/v2/consumer"
+	"github.com/pact-foundation/pact-go/v2/log"
+	"github.com/pact-foundation/pact-go/v2/matchers"
+	"github.com/stretchr/testify/assert"
+)
+
+var Like = matchers.Like
+var EachLike = matchers.EachLike
+var Term = matchers.Term
+var Regex = matchers.Regex
+var HexValue = matchers.HexValue
+var Identifier = matchers.Identifier
+var IPAddress = matchers.IPAddress
+var IPv6Address = matchers.IPv6Address
+var Timestamp = matchers.Timestamp
+var Date = matchers.Date
+var Time = matchers.Time
+var UUID = matchers.UUID
+var ArrayMinLike = matchers.ArrayMinLike
+
+type S = matchers.S
+type Map = matchers.MapMatcher
+
+var u *url.URL
+var client *consumer1.Client
+
+func TestClientPact_GetUser(t *testing.T) {
+
+	log.SetLogLevel("INFO")
+	mockProvider, err := consumer.NewV2Pact(consumer.MockHTTPProviderConfig{
+		Consumer: os.Getenv("CONSUMER_NAME"),
+		Provider: os.Getenv("PROVIDER_NAME"),
+		LogDir:   os.Getenv("LOG_DIR"),
+		PactDir:  os.Getenv("PACT_DIR"),
+	})
+	assert.NoError(t, err)
+
+	t.Run("Product exist", func(t *testing.T) {
+		id := 10
+
+		err = mockProvider.
+			AddInteraction().
+			Given("Product exists").
+			UponReceiving("A request to get product").
+			WithRequestPathMatcher("GET", Regex("/api/v1/product/"+strconv.Itoa(id), "/api/v1/products/[0-9]+")).
+			WillRespondWith(200, func(b *consumer.V2ResponseBuilder) {
+				b.BodyMatch(model.Product{}).
+					Header("Content-Type", Term("application/json", `application\/json`))
+			}).
+			ExecuteTest(t, func(config consumer.MockServerConfig) error {
+				// Act: test our API client behaves correctly
+
+				// Get the Pact mock server URL
+				u, _ = url.Parse("http://" + config.Host + ":" + strconv.Itoa(config.Port))
+
+				// Initialise the API client and point it at the Pact mock server
+				client = &consumer1.Client{
+					BaseURL: u,
+				}
+
+				// Execute the API client
+				product, err := client.GetProduct(id)
+
+				// Assert
+				if product.ID != id {
+					return fmt.Errorf("wanted product with ID %d but got %d", id, product.ID)
+				}
+
+				return err
+			})
+
+		assert.NoError(t, err)
+
+	})
+
+	t.Run("the product does not exist", func(t *testing.T) {
+		id := 10
+
+		err = mockProvider.
+			AddInteraction().
+			Given("Product does not exist").
+			UponReceiving("A request to get a product that does not exist").
+			WithRequestPathMatcher("GET", Regex("/api/v1/product/"+strconv.Itoa(id), "/api/v1/products/[0-9]+")).
+			WillRespondWith(404, func(b *consumer.V2ResponseBuilder) {
+				b.Header("Content-Type", Term("application/json", `application\/json`))
+			}).
+			ExecuteTest(t, func(config consumer.MockServerConfig) error {
+				// Get the Pact mock server URL
+				u, _ = url.Parse("http://" + config.Host + ":" + strconv.Itoa(config.Port))
+
+				// Initialise the API client and point it at the Pact mock server
+				client = &consumer1.Client{
+					BaseURL: u,
+				}
+
+				// Act: Execute the API client
+				_, err := client.GetProduct(id)
+
+				// Assert
+				assert.Equal(t, model.ErrNotFound, err)
+				return nil
+			})
+		assert.NoError(t, err)
+
+	})
+
+}
--- contract_test/consumer1/go.mod
+++ contract_test/consumer1/go.mod
@@ -1,3 +1,27 @@
 module consumer1
 
 go 1.25.0
+
+require (
+	github.com/pact-foundation/pact-go/v2 v2.4.1
+	github.com/pact-foundation/pact-workshop-go v0.0.0-20250626002734-580e7f564dff
+	github.com/stretchr/testify v1.11.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/hashicorp/go-version v1.7.0 // indirect
+	github.com/hashicorp/logutils v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/afero v1.12.0 // indirect
+	github.com/spf13/cobra v1.9.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	google.golang.org/grpc v1.71.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
